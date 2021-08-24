caa58db68e30b6b2a8db7195be14372af6d8e3f3
ipmanlk <io@navinda.xyz>
2021-08-24 14:26:44 +0530

    add: candiate profile ui interactions

--- pages/candidate_profile/candidate_profile.js
+++ pages/candidate_profile/candidate_profile.js
@@ -12,32 +12,137 @@ async function loadModule(permissionStr) {
 
 	// create an array from permission string
 	const permission = permissionStr.split("").map((p) => parseInt(p));
+
+	$("*").on("click", (e) => {
+		e.preventDefault();
+	});
+
+	$("#btnAddToEduList").click((e) => {
+		addToEducationList();
+	});
+
+	$("#btnAddToProList").click((e) => {
+		addToProList();
+	});
+
+	$("#btnAddToExpList").click((e) => {
+		addToExpList();
+	});
 }
 
 /*-------------------------------------------------------------------------------------------------------
-                                            Modals
+                                           Handle form interactions
 -------------------------------------------------------------------------------------------------------*/
-
-const showNewEntryModal = () => {
-	$("#modalMainFormTitle").text("Add New Role");
-	$("#modalMainForm").modal("show");
+const updateListRemoveButtonListeners = () => {
+	$(".btn-remove-from-list").off();
+	$(".btn-remove-from-list").click((e) => {
+		removeFromList(e);
+	});
 };
 
-const showEditEntryModal = async (id, readOnly = false) => {
-	if (readOnly) {
-		$("#modalMainFormTitle").text("View Role");
-	} else {
-		$("#modalMainFormTitle").text("Edit Role");
-	}
+const addToEducationList = () => {
+	$("#listEduQualifications").append(`
+	<div class="row">
+	<div class="col-xs-5">
+		<div class="form-group has-feedback">
+			<label>Name:</label>
+			<input type="text" class="form-control" placeholder="Qualification name"/>
+		</div>
+	</div>
+	<div class="col-xs-5">
+		<div class="form-group has-feedback">
+			<label>Image / Proof:</label>
+			<input type="file" class="form-control" />
+		</div>
+	</div>
+	<div class="col-xs-2">
+		<div class="form-group" style="float: right">
+			<label style="color: white">button</label>
+			<input
+				type="button"
+				class="btn btn-danger btn-remove-from-list"
+				style="display: block"
+				value="x"
+			/>
+		</div>
+	</div>
+</div>
+	`);
 
-	$("#modalMainForm").modal("show");
+	updateListRemoveButtonListeners();
 };
+
+const addToProList = () => {
+	$("#listProQualifications").append(`
+	<div class="row">
+	<div class="col-xs-5">
+		<div class="form-group has-feedback">
+			<label>Name:</label>
+			<input type="text" class="form-control" placeholder="Qualification name"/>
+		</div>
+	</div>
+	<div class="col-xs-5">
+		<div class="form-group has-feedback">
+			<label>Image / Proof:</label>
+			<input type="file" class="form-control" />
+		</div>
+	</div>
+	<div class="col-xs-2">
+		<div class="form-group" style="float: right">
+			<label style="color: white">button</label>
+			<input
+				type="button"
+				class="btn btn-danger btn-remove-from-list"
+				style="display: block"
+				value="x"
+			/>
+		</div>
+	</div>
+</div>
+	`);
+
+	updateListRemoveButtonListeners();
+};
+
+const addToExpList = () => {
+	$("#listExperiences").append(`
+	<div class="row">
+	<div class="col-xs-5">
+		<div class="form-group has-feedback">
+			<label>Title:</label>
+			<input type="text" class="form-control" placeholder="Experience title"/>
+		</div>
+	</div>
+	<div class="col-xs-5">
+		<div class="form-group has-feedback">
+			<label>Image / Proof:</label>
+			<input type="file" class="form-control" />
+		</div>
+	</div>
+	<div class="col-xs-2">
+		<div class="form-group" style="float: right">
+			<label style="color: white">button</label>
+			<input
+				type="button"
+				class="btn btn-danger btn-remove-from-list"
+				style="display: block"
+				value="x"
+			/>
+		</div>
+	</div>
+</div>
+	`);
+
+	updateListRemoveButtonListeners();
+};
+
+const removeFromList = (event) => {
+	$(event.target).parent().parent().parent().remove();
+	updateListRemoveButtonListeners();
+};
+
+const getFormData = () => {};
 
 /*-------------------------------------------------------------------------------------------------------
                                             Operations
 -------------------------------------------------------------------------------------------------------*/
-// add new entry to the database
-const addEntry = async () => {};
-
-// update entry in the database
-const updateEntry = async () => {};
