116891eb639f41c22ad04df164ccb69a609cd130
ipmanlk <io@navinda.xyz>
2021-09-07 20:49:37 +0530

    add: candidate review profile

--- pages/candidate_profile_review/candidate_profile_review.js
+++ pages/candidate_profile_review/candidate_profile_review.js
@@ -1,3 +1,5 @@
+const tempData = {};
+
 /*-------------------------------------------------------------------------------------------------------
                                             General
 -------------------------------------------------------------------------------------------------------*/
@@ -26,7 +28,7 @@ async function loadModule(permissionStr) {
 				Name: entry.shortName,
 				NIC: entry.nic,
 				Mobile: entry.mobile,
-				ModifiedDate: entry.ModifiedDate,
+				"Modified Date": entry.modifiedDate,
 				"Review Status": entry.candidateProfileReviewStatus.name,
 				Review: `<button class="btn btn-success btn-sm" onclick="showEditEntryModal('${entry.id}')"><i class="glyphicon glyphicon-eye-open" aria-hidden="true"></i> Review</button>`,
 			};
@@ -42,6 +44,8 @@ async function loadModule(permissionStr) {
 	);
 
 	loadFormDropdowns();
+
+	$("#btnUpdateProfile").click(updateCandidateProfile);
 }
 
 const loadFormDropdowns = async () => {
@@ -57,6 +61,35 @@ const loadFormDropdowns = async () => {
 		$("#candidateProfileReviewStatusId").append(
 			`<option value="${s.id}">${s.name}</option>`
 		);
+	});
+};
+
+const updateEventListeners = () => {
+	$(".qua-score").off();
+
+	$(".qua-score").on("change keyup", (e) => {
+		const targetVal = $(e.target).val() || 0;
+
+		if (targetVal === 0) {
+			$(e.target).val(0);
+		} else if (targetVal > 10) {
+			$(e.target).val(10);
+		} else if (targetVal < 0) {
+			$(e.target).val(0);
+		}
+
+		let total = 0;
+		let count = 0;
+		$(".qua-score").each((index, el) => {
+			const value = $(el).val() || 0;
+			if (value === 0) return;
+			total += parseInt(value);
+			count += 1;
+		});
+
+		const perc = (total / (count * 10)) * 100;
+
+		$("#reviewScore").val(Math.round(perc).toString() + "%");
 	});
 };
 
@@ -78,6 +111,8 @@ const showEditEntryModal = async (id) => {
 		$(`#${key}`).val(entry[key]);
 	});
 
+	$("#reviewScore").val(entry.reviewScore + "%");
+
 	$("#nicProof").html(
 		`<a href="http://localhost:3000/uploads/${entry.nicFilename}" target="_blank">View</a>`
 	);
@@ -88,6 +123,14 @@ const showEditEntryModal = async (id) => {
 		`http://localhost:3000/uploads/${entry.photoFilename}`
 	);
 
+	$("#listEduQualifications").parent().hide();
+	$("#listProQualifications").parent().hide();
+	$("#listExperiences").parent().hide();
+
+	$("#listEduQualifications").empty();
+	$("#listProQualifications").empty();
+	$("#listExperiences").empty();
+
 	entry.candidateQualifications.forEach((q) => {
 		if (q.qualificationType.name == "Educational") {
 			$("#listEduQualifications").append(`
@@ -96,7 +139,7 @@ const showEditEntryModal = async (id) => {
 				<div class="form-group has-feedback">
 					<label>Name:</label>
 					<br/>
-					<span>${q.name}"</span>
+					<span>${q.name}</span>
 				</div>
 			</div>
 		
@@ -113,12 +156,13 @@ const showEditEntryModal = async (id) => {
 				<div class="form-group has-feedback">
 					<label>Score:</label>
 					<br/>
-					<input type="number" data-qid="${q.id}" placeholder="5"/>
+					<input type="number" class="qua-score" data-qid="${q.id}" placeholder="5" min="0" max="10" value="${q.score}"/>
 				</div>
 			</div>
 
 		</div>
 			`);
+			$("#listEduQualifications").parent().show();
 		} else if (q.qualificationType.name == "Professional") {
 			$("#listProQualifications").append(`
 			<div class="row">
@@ -126,7 +170,7 @@ const showEditEntryModal = async (id) => {
 				<div class="form-group has-feedback">
 					<label>Name:</label>
 					<br/>
-					<span>${q.name}"</span>
+					<span>${q.name}</span>
 				</div>
 			</div>
 		
@@ -143,20 +187,22 @@ const showEditEntryModal = async (id) => {
 				<div class="form-group has-feedback">
 					<label>Score:</label>
 					<br/>
-					<input type="number" data-qid="${q.id}" placeholder="5"/>
+					<input type="number" class="qua-score" data-qid="${q.id}" placeholder="5" min="0" max="10" value="${q.score}"/>
 				</div>
 			</div>
 
 		</div>
 			`);
+
+			$("#listProQualifications").parent().show();
 		} else {
-			$("#listProQualifications").append(`
+			$("#listExperiences").append(`
 			<div class="row">
 			<div class="col-xs-6">
 				<div class="form-group has-feedback">
 					<label>Name:</label>
 					<br/>
-					<span>${q.name}"</span>
+					<span>${q.name}</span>
 				</div>
 			</div>
 		
@@ -173,12 +219,14 @@ const showEditEntryModal = async (id) => {
 				<div class="form-group has-feedback">
 					<label>Score:</label>
 					<br/>
-					<input type="number" data-qid="${q.id}" placeholder="5"/>
+					<input type="number" class="qua-score" data-qid="${q.id}" placeholder="5" min="0" max="10" value="${q.score}"/>
 				</div>
 			</div>
 
 		</div>
 			`);
+
+			$("#listExperiences").parent().show();
 		}
 	});
 
@@ -188,4 +236,58 @@ const showEditEntryModal = async (id) => {
 	);
 	$("#modalMainFormTitle").text(`Candidate Profile of ${entry.shortName}`);
 	$("#modalMainForm").modal("show");
+
+	tempData.selectedEntry = entry;
+
+	updateEventListeners();
+};
+
+/*-------------------------------------------------------------------------------------------------------
+                                            Operations
+-------------------------------------------------------------------------------------------------------*/
+const updateCandidateProfile = async () => {
+	const candidateProfileId = tempData.selectedEntry.id;
+	const qualificationScores = getQualificationScores();
+	const candidateProfileReviewStatusId = $(
+		"#candidateProfileReviewStatusId"
+	).val();
+
+	const data = {
+		candidateProfileId,
+		qualificationScores,
+		candidateProfileReviewStatusId,
+	};
+
+	const response = await Request.send("/api/candidate_profile_reviews", "PUT", {
+		data: data,
+	});
+
+	mainTable.reload();
+	$("#modalMainForm").modal("hide");
+
+	if (response.status) {
+		mainWindow.showOutputToast("Success!", response.msg);
+	}
+};
+
+const getQualificationScores = () => {
+	const qualificationSelectors = {
+		"#listEduQualifications": "Educational",
+		"#listProQualifications": "Professional",
+		"#listExperiences": "Experience",
+	};
+
+	const qScores = [];
+
+	for (const selector of Object.keys(qualificationSelectors)) {
+		for (const row of $(selector).children(".row").toArray()) {
+			const numInput = $(row).find("input[type='number']").first();
+			const id = parseInt(numInput.data("qid"));
+			const score = parseInt(numInput.val());
+
+			qScores.push({ id, score });
+		}
+	}
+
+	return qScores;
 };
