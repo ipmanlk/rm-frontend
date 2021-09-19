074b8a62a6ea1a9e6c8b425f7542d380aedae002
ipmanlk <io@navinda.xyz>
2021-09-19 11:28:48 +0530

    add: model for job application review with 3 tabs

--- pages/job_application_review/job_application_review.js
+++ pages/job_application_review/job_application_review.js
@@ -64,6 +64,9 @@ async function loadModule(permissionStr) {
 		submitApplication();
 	});
 
+	// init datetime picker
+	$("#idateTime").datetimepicker();
+
 	// catch promise rejections
 	$(window).on("unhandledrejection", (event) => {
 		console.error(
@@ -101,29 +104,82 @@ const showNewEntryModal = () => {
 
 const showEditEntryModal = async (id, readOnly = false) => {
 	// get entry data from db and show in the form
-	const response = await Request.send("/api/job_applications", "GET", {
+	const response = await Request.send("/api/job_application_reviews", "GET", {
 		data: { id: id },
 	});
 	const entry = response.data;
 	tempData.selectedEntry = entry;
 
-	mainForm.loadEntry(entry);
+	loadEntry(entry);
 
-	if (readOnly) {
-		mainForm.enableReadOnly();
-		$("#modalMainFormTitle").text("View Job Vacancy");
-	} else {
-		mainForm.disableReadOnly();
-		$("#modalMainFormTitle").text("Edit Job Vacancy");
-	}
+	$("#modalMainFormTitle").text("Job Application");
 
-	if (tempData.selectedEntry.applied) {
-		$("#btnApply").attr("disabled", true);
-	} else {
-		$("#btnApply").attr("disabled", false);
-	}
+	// if (tempData.selectedEntry.applied) {
+	// 	$("#btnApply").attr("disabled", true);
+	// } else {
+	// 	$("#btnApply").attr("disabled", false);
+	// }
 
 	$("#modalMainForm").modal("show");
+};
+
+const loadEntry = (entry) => {
+	Object.keys(entry.jobVacancy).forEach((k) => {
+		$(`#jv${k}`).text(entry.jobVacancy[k]);
+	});
+	$("#jvdepartment").text(entry.jobVacancy.department.name);
+	$("#jvvacancyStatus").text(entry.jobVacancy.jobVacancyStatus.name);
+	$("#jvcategory").text(entry.jobVacancy.jobCategory.name);
+
+	Object.keys(entry.candidateProfile).forEach((k) => {
+		$(`#cp${k}`).text(entry.candidateProfile[k]);
+	});
+
+	$("#candidateProfileReviewStatus").text(
+		entry.candidateProfile.candidateProfileReviewStatus.name
+	);
+
+	$("#cpnicProof").html(
+		`<a href="http://localhost:3000/uploads/${entry.candidateProfile.nicFilename}" target="_blank">View</a>`
+	);
+
+	$("#cpphotoPreview").attr(
+		"src",
+		`http://localhost:3000/uploads/${entry.candidateProfile.photoFilename}`
+	);
+
+	const tableHTML =
+		"<table class='table table-bordered'><thead><tr><th>Name</th><th>Score</th><th>Proof</th></tr></thead><tbody><tbody></table>";
+
+	$("#listEduQualifications").html(tableHTML);
+	$("#listProQualifications").html(tableHTML);
+	$("#listExperiences").html(tableHTML);
+
+	$("#listEduQualifications").hide();
+	$("#listProQualifications").hide();
+	$("#listExperiences").hide();
+
+	entry.candidateProfile.candidateQualifications.forEach((q) => {
+		let target;
+		if (q.qualificationType.name == "Educational") {
+			target = "#listEduQualifications";
+			$("#listEduQualifications").show();
+		} else if (q.qualificationType.name == "Professional") {
+			target = "#listProQualifications";
+			$("#listProQualifications").show();
+		} else {
+			target = "#listExperiences";
+			$("#listExperiences").show();
+		}
+
+		const row = `<tr>
+			<td>${q.name}</td>
+			<td>${q.score}</td>
+			<td><a href="http://localhost:3000/uploads/${q.filename}" target="_blank">View</a></td>
+		<tr>`;
+
+		$(`${target} tbody`).append(row);
+	});
 };
 
 /*-------------------------------------------------------------------------------------------------------
@@ -138,7 +194,7 @@ const submitApplication = async () => {
 	if (!confirmation) return;
 
 	// get response
-	const response = await Request.send("/api/job_applications", "POST", {
+	const response = await Request.send("/api/job_application_reviews", "POST", {
 		data: {
 			jobVacancyId: tempData.selectedEntry.id,
 		},
